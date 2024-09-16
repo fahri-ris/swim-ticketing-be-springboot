004e04113d7e4be30145499d56385ce37d75dc67
Fahri Ris <fahri.ris15@gmail.com>
2024-09-17 01:48:29 +0700

    [BaseResponse] add one attribute

--- src/main/java/com/ris/swimticketing/controller/TicketOrderController.java
+++ src/main/java/com/ris/swimticketing/controller/TicketOrderController.java
@@ -5,6 +5,7 @@ import com.ris.swimticketing.dto.TicketOrderRequest;
 import com.ris.swimticketing.service.TicketOrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +27,6 @@ public class TicketOrderController {
     public ResponseEntity<Object> addTicketOrder(@RequestBody TicketOrderRequest ticketOrderRequest) {
         log.info("masuk");
         Object response = ticketOrderService.addTicketOrder(ticketOrderRequest);
-        return ResponseEntity.ok(BaseResponse.success(response, "Ticket order successfully"));
+        return ResponseEntity.ok(BaseResponse.success(response, "Ticket order successfully", HttpStatus.OK.value()));
     }
 }
--- src/main/java/com/ris/swimticketing/dto/BaseResponse.java
+++ src/main/java/com/ris/swimticketing/dto/BaseResponse.java
@@ -11,22 +11,25 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BaseResponse<T> {
     private boolean success;
+    private Integer statusCode;
     private String message;
     private Long time;
     private T data;
 
-    public static<T> BaseResponse<T> success(T data, String message) {
+    public static<T> BaseResponse<T> success(T data, String message, Integer statusCode) {
         return BaseResponse.<T>builder()
                 .success(true)
+                .statusCode(statusCode)
                 .message(message)
                 .time(System.currentTimeMillis())
                 .data(data)
                 .build();
     }
 
-    public static<T> BaseResponse<T> error(T data, String message) {
+    public static<T> BaseResponse<T> error(T data, String message, Integer statusCode) {
         return BaseResponse.<T>builder()
                 .success(false)
+                .statusCode(statusCode)
                 .message(message)
                 .time(System.currentTimeMillis())
                 .data(data)
