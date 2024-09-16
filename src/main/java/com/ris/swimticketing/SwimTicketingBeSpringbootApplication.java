b8804f7bb5f317a0a90b9e6a020ad153540512d5
Fahri Ris <fahri.ris15@gmail.com>
2024-09-16 15:27:31 +0700

    init

---
+++ src/main/java/com/ris/swimticketing/SwimTicketingBeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ris.swimticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimTicketingBeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimTicketingBeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ris/swimticketing/SwimTicketingBeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ris.swimticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimTicketingBeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
